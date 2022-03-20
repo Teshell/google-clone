9ca45aa5c7ad18ad84cd47113277e66ec6b98750
Teshell <fayrongeek@gmail.com>
2022-03-20 11:28:43 +0100

    api key hidden

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   images: {
     domains: ["assets.stickpng.com"],
   },
-  env: {
-    API_KEY: "AIzaSyDmSbl3q-ywHm_ToUhnagbIx2xILuS_smc",
-    CONTEXT_KEY: "4609ac6ac7bf16525",
-  },
+
   reactStrictMode: true,
 };
 
